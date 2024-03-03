665a0d3740dbc327743d5a0447ec31e1dd8e435f
Nitish Agarwal <nitishagarwal1737@gmail.com>
2024-03-03 20:41:21 +0530

    sidebar done

--- client/src/components/sidebar/links/Links.js
+++ client/src/components/sidebar/links/Links.js
@@ -1,15 +1,44 @@
 import React from 'react'
 import "../Sidebar.css";
+import {motion} from "framer-motion"
+
+const variants = {
+    open: {
+        transition:{
+            staggerChildren: 0.1,
+        },
+    },
+    closed:{
+        transition:{
+            staggerChildren:0.05,
+            staggerDirection: -1,
+        }
+    }
+}
+
+const itemVariants = {
+    open: {
+       y:0,
+       opacity: 1,
+    },
+    closed:{
+       y:50,
+       opacity: 0,
+    }
+}
 
 const Links = () => {
 
     const items = ["Home" , "Projects", "Contact" , "About"]
   return (
-    <div className='Links absolute w-full h-full flex flex-col items-center justify-center gap-5'>
+    <motion.div className='Links absolute w-full h-full flex flex-col items-center justify-center gap-5'
+    variants={variants}>
       {items.map((item) =>(
-        <a href={`#${item}`} key={item}> {item}</a>
+        <motion.a href={`#${item}`} key={item} variants={itemVariants}
+        whileHover={{scale:1.2}}
+        whileTap={{scale:0.9}}> {item}</motion.a>
       ))}
-    </div>
+    </motion.div>
   )
 }
 
--- client/src/components/sidebar/togglebutton/ToggleButton.js
+++ client/src/components/sidebar/togglebutton/ToggleButton.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "../Sidebar.css"
+import {motion} from "framer-motion"
 
 const ToggleButton = ({setOpen}) => {
   return (
@@ -7,7 +8,24 @@ const ToggleButton = ({setOpen}) => {
     type='button'
     className='w-12 h-12 fixed top-6 left-6 bg-transparent border-none cursor-pointer'
     onClick={() => setOpen((prev)=>!prev)}>
-      Button
+      <svg width="23" height="23" viewBox='0 0 23 23'>
+        <motion.path strokeWidth="3" stroke='black' strokeLinecap='round'
+        variants={{
+            closed: {d: "M 2 2.5 L 20 2.5"},
+            open: {d: "M 3 16.5 L 17 2.5"}
+        }}/>
+        <motion.path strokeWidth="3" stroke='black' strokeLinecap='round' d="M 2 9.423 L 20 9.423"
+        variants={{
+            open: {opacity:0},
+            closed: {opacity:1},
+        }}
+        />
+        <motion.path strokeWidth="3" stroke='black' strokeLinecap='round'
+        variants={{
+            closed: {d: "M 2 16.346 L 20 16.346"},
+            open: {d: "M 3 2.5 L 17 16.346"}
+        }}/>
+      </svg>
     </button>
   )
 }
