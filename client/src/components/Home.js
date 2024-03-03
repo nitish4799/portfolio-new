271790240f96124d3ac4ed5bfcebdf26c0842693
Nitish Agarwal <nitishagarwal1737@gmail.com>
2024-03-03 10:37:46 +0530

    first commit

---
+++ client/src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <div className='hero-full-text-panel'>
+            <div className='hero-hello-text'>
+                <div className='perspective-load1'></div>
+                <h1 className='hero-text text-7xl'>Hi</h1>
+            </div>
+            <div className='hero-text-content'>
+                <h1 className='text-7xl'>I'm Nitish</h1>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
