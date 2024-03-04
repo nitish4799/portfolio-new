157d9a71390a66da320663c75adcf0657eef437f
Nitish Agarwal <nitishagarwal1737@gmail.com>
2024-03-04 15:51:11 +0530

    contact page added

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
 import Parallax from './components/parallax/Parallax';
 import Portfolio from './components/portfolio/Portfolio';
+import Contact from './components/contact/Contact';
 // import Home from './components/Home';
 
 
@@ -19,7 +20,7 @@ function App() {
       </section>
       <section id='Services'><Parallax type="services"/></section>
       <Portfolio/>
-      <section id='Contact'>Contact</section>
+      <section id='Contact'><Contact/></section>
       {/* <Test/>
       <Test/> */}
     </>
